3a3c96e79b66e4925be5bbcfc85db9cba80400d3
kusthanapat <102140251+kusthanapat@users.noreply.github.com>
2024-11-10 13:06:31 +0700

    mydurian-master

---
+++ pages/chart.vue
@@ -0,0 +1,12 @@
+<script setup>
+    // import VueApexCharts from 'vue-apexcharts'
+    definePageMeta({
+        layout: "defaultuser"
+    })
+</script>
+
+<template>
+    <div>
+    chart
+    </div>
+</template>
